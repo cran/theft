71048b87d09c5c9fd06e7b59db05f82822d92f04
Trent Henderson <then6675@uni.sydney.edu.au>
2025-07-10 07:10:02 +0000

    version 0.8.1

--- inst/doc/theft.R
+++ inst/doc/theft.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -11,42 +11,24 @@ knitr::opts_chunk$set(
 ## ----setup, message = FALSE, warning = FALSE----------------------------------
 library(theft)
 
-## ---- message = FALSE, warning = FALSE, eval = FALSE--------------------------
-#  theft::simData
+## ----message = FALSE, warning = FALSE, eval = FALSE---------------------------
+# theft::simData
 
-## ---- message = FALSE, warning = FALSE----------------------------------------
+## ----message = FALSE, warning = FALSE-----------------------------------------
 head(simData)
 
-## ---- eval = FALSE------------------------------------------------------------
-#  install_python_pkgs("theft-package")
+## ----eval = FALSE-------------------------------------------------------------
+# install_python_pkgs(venv = "theft-package", python = "/usr/local/bin/python3.10")
 
-## ---- eval = FALSE------------------------------------------------------------
-#  init_theft("theft-package")
+## ----eval = FALSE-------------------------------------------------------------
+# init_theft("theft-package")
 
-## ---- message = FALSE, warning = FALSE----------------------------------------
-feature_matrix <- calculate_features(data = simData, 
-                                     id_var = "id", 
-                                     time_var = "timepoint", 
-                                     values_var = "values", 
-                                     group_var = "process", 
-                                     feature_set = "catch22",
-                                     seed = 123)
-
+## ----message = FALSE, warning = FALSE-----------------------------------------
+feature_matrix <- calculate_features(data = simData, feature_set = "catch22")
 head(feature_matrix)
 
-## ---- message = FALSE, warning = FALSE, eval = FALSE--------------------------
-#  feature_matrix <- calculate_features(data = simData,
-#                                       id_var = "id",
-#                                       time_var = "timepoint",
-#                                       values_var = "values",
-#                                       group_var = "process",
-#                                       feature_set = "catch22",
-#                                       catch24 = TRUE,
-#                                       seed = 123)
-
-## ---- message = FALSE, warning = FALSE----------------------------------------
+## ----message = FALSE, warning = FALSE-----------------------------------------
 feature_matrix2 <- calculate_features(data = simData, 
-                                      group_var = "process",
                                       feature_set = NULL,
                                       features = list("mean" = mean, "sd" = sd))
 
